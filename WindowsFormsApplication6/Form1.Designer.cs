47211adccf66bd3031b331e0b845f0477317b7b6
Bryan Cho <bryancho31@gmail.com>
2016-11-16 17:51:24 -0600

    Init

---
+++ WindowsFormsApplication6/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace WindowsFormsApplication6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Telerik.WinControls.UI.TableViewDefinition tableViewDefinition2 = new Telerik.WinControls.UI.TableViewDefinition();
+            this.radGridView1 = new Telerik.WinControls.UI.RadGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radGridView1
+            // 
+            this.radGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.radGridView1.Location = new System.Drawing.Point(0, 0);
+            // 
+            // 
+            // 
+            this.radGridView1.MasterTemplate.ViewDefinition = tableViewDefinition2;
+            this.radGridView1.Name = "radGridView1";
+            this.radGridView1.Size = new System.Drawing.Size(1278, 775);
+            this.radGridView1.TabIndex = 0;
+            this.radGridView1.Text = "radGridView1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1278, 775);
+            this.Controls.Add(this.radGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadGridView radGridView1;
+    }
+}
+
---
+++ WindowsFormsApplication6/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Telerik.WinControls.UI; 
+
+
+namespace WindowsFormsApplication6
+{
+    public partial class Form1 : Form
+    {
+        AdventureWorksDataSet adventureWorksDS;
+        AdventureWorksDataSetTableAdapters.SalesPersonTableAdapter salesPersonTA;
+        AdventureWorksDataSetTableAdapters.SalesTerritoryTableAdapter salesTerritoryTA; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            adventureWorksDS = new AdventureWorksDataSet();
+            salesPersonTA = new AdventureWorksDataSetTableAdapters.SalesPersonTableAdapter();
+            salesTerritoryTA = new AdventureWorksDataSetTableAdapters.SalesTerritoryTableAdapter(); 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radGridView1.DataSource = adventureWorksDS.SalesTerritory;
+            radGridView1.MasterTemplate.AutoSizeColumnsMode = GridViewAutoSizeColumnsMode.Fill;
+            radGridView1.ThemeName = "VistaOrange";
+            radGridView1.MasterTemplate.AllowAddNewRow = false;
+            radGridView1.Columns["rowguid"].IsVisible = false;
+
+            GridViewTemplate childTmpt = new GridViewTemplate();
+            childTmpt.DataSource = adventureWorksDS.SalesPerson;
+            childTmpt.AutoSizeColumnsMode = GridViewAutoSizeColumnsMode.Fill;
+            childTmpt.AllowAddNewRow = false;
+            radGridView1.MasterTemplate.Templates.Add(childTmpt);
+
+            GridViewRelation relation = new GridViewRelation(radGridView1.MasterTemplate);
+            relation.ChildTemplate = childTmpt;
+            relation.RelationName = "SalesTerritoryPerson";
+            relation.ParentColumnNames.Add("TerritoryID");
+            relation.ChildColumnNames.Add("TerritoryID");
+            radGridView1.Relations.Add(relation);
+
+            salesTerritoryTA.Fill(adventureWorksDS.SalesTerritory);
+            salesPersonTA.Fill(adventureWorksDS.SalesPerson);
+            childTmpt.Columns["rowguid"].IsVisible = false; 
+        }
+    }
+}
